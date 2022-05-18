6ae815b02568fce90affd23eee25663925def7e5
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-05-18 17:32:51 +0200

    Added sub classes NumericInputError and TextInputError

--- Uppgift3a/UserError.cs
+++ Uppgift3a/UserError.cs
@@ -8,6 +8,23 @@ namespace Uppgift3a
 {
     internal abstract class UserError
     {
+        public abstract string UEMessage();
+    }
 
+    // sub classes
+    internal class NumericInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a numeric input in a text only field. This fired an error!";
+        }
+    }
+
+    internal class TextInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a text input in a numeric only field. This fired an error!";
+        }
     }
 }
