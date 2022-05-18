49cf97649492c2a0d57f6a0d360430e0f44df703
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-05-18 16:11:29 +0200

    Added Animal sub classes.

--- Uppgift3a/Animal.cs
+++ Uppgift3a/Animal.cs
@@ -11,19 +11,75 @@ namespace Uppgift3a
         public string Name { get; set; }
         public double Weight { get; set; }
         public int Age { get; set; }
-        public string Sound { private get; set; }
 
-        public Animal(string name, double weight, int age, string sound)
+        public Animal(string name, double weight, int age)
         {
             Name = name;
             Weight = weight;
             Age = age;
-            Sound = sound;
         }
 
-        public string DoSound()
+        public abstract string DoSound();
+    }
+
+    internal class Horse : Animal
+    {
+        public Horse(string name, double weight, int age) : base(name, weight, age) { }
+
+        public override string DoSound()
         {
-            return Sound;
+            return "Neigh";
+        }
+    }
+
+    internal class Dog : Animal
+    {
+        public Dog(string name, double weight, int age) : base(name, weight, age) { }
+
+        public override string DoSound()
+        {
+            return "Woof";
+        }
+    }
+
+    internal class Hedgehog : Animal
+    {
+        public Hedgehog(string name, double weight, int age) : base(name, weight, age) { }
+
+        public override string DoSound()
+        {
+            return "Hedgehog sound";
+        }
+
+    }
+
+    internal class Worm : Animal
+    {
+        public Worm(string name, double weight, int age) : base(name, weight, age) { }
+
+        public override string DoSound()
+        {
+            return "Worm sound";
+        }
+    }
+
+    internal class Bird : Animal
+    {
+        public Bird(string name, double weight, int age) : base(name, weight, age) { }
+
+        public override string DoSound()
+        {
+            return "Squeek";
+        }
+    }
+
+    internal class Wolf : Animal
+    {
+        public Wolf(string name, double weight, int age) : base(name, weight, age) { }
+
+        public override string DoSound()
+        {
+            return "Houl";
         }
     }
 }
