0e52522dac8834a8cba73e2cdc87b311a8b6b396
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-05-18 15:51:06 +0200

    Added Animal class

---
+++ Uppgift3a/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift3a
+{
+    internal abstract class Animal
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public int Age { get; set; }
+        public string Sound { private get; set; }
+
+        public Animal(string name, double weight, int age, string sound)
+        {
+            Name = name;
+            Weight = weight;
+            Age = age;
+            Sound = sound;
+        }
+
+        public string DoSound()
+        {
+            return Sound;
+        }
+    }
+}
