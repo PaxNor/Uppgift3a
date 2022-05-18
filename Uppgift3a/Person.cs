a08ed6a4115473514ba8a088cb3d4fcfb0e9508a
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-05-18 14:12:17 +0200

    Add project files.

---
+++ Uppgift3a/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift3a
+{
+    internal class Person
+    {
+        private uint age;
+        private string fName;
+        private string lName;
+        private uint height;
+        private uint weight;
+
+        public uint Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string FName
+        {
+            get { return fName; }
+            set { fName = value; }
+        }
+
+        public string LName
+        {
+            get { return lName; }
+            set { lName = value; }
+        }
+
+        public uint Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        public uint Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+
+
+
+    }
+}
