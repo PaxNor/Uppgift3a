2f53d59da67cf5322aa7d76ebe33da21cbfd0df9
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-05-18 15:06:53 +0200

    Added the rest of the methods to PersonHandler

--- Uppgift3a/PersonHandler.cs
+++ Uppgift3a/PersonHandler.cs
@@ -22,5 +22,55 @@ namespace Uppgift3a
 
             return person;
         }
+
+        public void SetPersonAge(Person person, uint age)
+        {
+            person.Age = age;
+        }
+
+        public uint GetPersonAge(Person person)
+        {
+            return person.Age;
+        }
+
+        public void ChangePersonFirstName(Person person, string newName)
+        {
+            person.FName = newName;
+        }
+
+        public string GetPersonFirstName(Person person)
+        {
+            return person.FName;
+        }
+
+        public void ChangePersonLastName(Person person, string newName)
+        {
+            person.LName = newName;
+        }
+
+        public string GetPersonLastName(Person person)
+        {
+            return person.LName;
+        }
+
+        public void ChangePersonWeight(Person person, double newWeight)
+        {
+            person.Weight = newWeight;
+        }
+
+        public double GetPersonWeight(Person person)
+        {
+            return person.Weight;
+        }
+
+        public void ChangePersonHeight(Person person, double newHeight)
+        {
+            person.Height = newHeight;
+        }
+
+        public double GetPersonHeight(Person person)
+        {
+            return person.Height;
+        }
     }
 }
